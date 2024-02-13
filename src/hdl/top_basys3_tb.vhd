d95759fc5bc77515834a9d56055c894dacb24d9b
Breen-J <141793138+Breen-J@users.noreply.github.com>
2024-02-13 11:34:53 -0700

    ICE 3 TB Fix V3... asserts

--- src/hdl/top_basys3_tb.vhd
+++ src/hdl/top_basys3_tb.vhd
@@ -93,23 +93,23 @@ begin
 	-- Implement the test plan here.  Body of process is continuously from time = 0  
 	test_process : process 
 	begin
-	
+	-- MSB = Cout... LSB = S
 	    w_sw <= o"0"; wait for 10 ns;
-		  assert w_led = "0" report "bad o0" severity failure;
+		  assert w_led = "00" report "bad o0" severity failure;
         w_sw <= o"1"; wait for 10 ns;
-          assert w_led = "1" report "bad o1" severity failure;
+          assert w_led = "01" report "bad o1" severity failure;
         w_sw <= o"2"; wait for 10 ns;
-          assert w_led = "1" report "bad o2" severity failure;
+          assert w_led = "01" report "bad o2" severity failure;
         w_sw <= o"3"; wait for 10 ns;
-          assert w_led = "0" report "bad o3" severity failure;
+          assert w_led = "10" report "bad o3" severity failure;
          w_sw <= o"4"; wait for 10 ns;
-          assert w_led = "1" report "bad o4" severity failure;
+          assert w_led = "01" report "bad o4" severity failure;
          w_sw <= o"5"; wait for 10 ns;
-          assert w_led = "0" report "bad o5" severity failure;          
+          assert w_led = "10" report "bad o5" severity failure;          
          w_sw <= o"6"; wait for 10 ns;
-          assert w_led = "0" report "bad o6" severity failure;
+          assert w_led = "10" report "bad o6" severity failure;
          w_sw <= o"7"; wait for 10 ns;
-          assert w_led = "1" report "bad o7" severity failure;        
+          assert w_led = "11" report "bad o7" severity failure;        
 	    --You must fill in the remaining test cases.	
 	
 		wait; -- wait forever
